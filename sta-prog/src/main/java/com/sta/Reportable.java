a5b58bbfb0c7716a43ca1b4dd1a4c1cc9a1a3a32
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2024-10-13 16:36:05 -0500

    An interface that generates report

---
+++ sta-prog/src/main/java/com/sta/Reportable.java
@@ -0,0 +1,9 @@
+package com.sta;
+
+interface Reportable {
+    // The reportable interface is used to print details of a class implemnting it
+
+    // The abstract method used to generate a report, the implementation of this method is in the implementing classes
+  public abstract String  generateReport();
+    
+}
