aab88e7d7b9af81e59eff34b4914aa9929f650e7
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2024-10-13 16:34:55 -0500

    A class that uses PDM to check source code

---
+++ sta-prog/src/main/java/com/sta/SourceAnalyzer.java
@@ -0,0 +1,17 @@
+package com.sta;
+
+public class SourceAnalyzer 
+{
+    public SourceAnalyzer(){
+
+    }
+
+    public void getSourceCode(String repoUrl){
+        
+    }
+    public static void main( String[] args )
+    {   
+        SourceAnalyzer analyser = new SourceAnalyzer();
+
+    }
+}
