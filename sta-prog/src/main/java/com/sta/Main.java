398500b7d2b6f21a633035763980d06186bff2f7
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2024-10-19 14:51:41 -0500

    Main class for launching the app

---
+++ sta-prog/src/main/java/com/sta/Main.java
@@ -0,0 +1,9 @@
+package com.sta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SourceAnalyzer source = new SourceAnalyzer();
+        GUI gui = new GUI(source);
+    }
+}
