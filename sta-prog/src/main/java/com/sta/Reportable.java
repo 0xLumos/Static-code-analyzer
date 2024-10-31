f135572f95fc7c62efadc7594fb72723be1a782b
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2024-10-31 14:55:06 -0500

    Deletes Reportable

--- sta-prog/src/main/java/com/sta/Reportable.java
+++
@@ -1,9 +0,0 @@
-package com.sat;
-
-interface Reportable {
-    // The reportable interface is used to print details of a class implemnting it
-
-    // The abstract method used to generate a report, the implementation of this method is in the implementing classes
-  public abstract String  generateReport();
-    
-}
