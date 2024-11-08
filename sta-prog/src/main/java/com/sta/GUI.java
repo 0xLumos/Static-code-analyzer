685fdb31325c1899e564753ebe551332a3132412
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2024-11-08 15:07:42 -0500

    Comment the code and apply necessary changes

--- sta-prog/src/main/java/com/sta/GUI.java
+++ sta-prog/src/main/java/com/sta/GUI.java
@@ -1,3 +1,4 @@
+
 package com.sat;
 
 import java.awt.BorderLayout;
@@ -30,7 +31,9 @@ import javax.swing.text.StyledDocument;
 
 class GUI {
     /* This class is responsible for initalizing the GUI, it takes a parameter of type SourceAnalyzer,
-    and display data such as local and remote repo paths, results file path, etc..*/
+    and display data such as local and remote repo paths, results file path, etc..
+    When the fetch&analyze button is pressed, the input will be checked to see if its a GitHub URL
+    If it is, clone it locally, and apply the PMD analysis from SourceAnalyzer() class */
 
     public GUI(SourceAnalyzer sourceLocation) {
         // Create the Frame
@@ -47,16 +50,17 @@ class GUI {
 
         // Add a JScrollPane to the text pane
         JScrollPane scrollPane = new JScrollPane(textPane);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);    // Vertical Scrollbar
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);    // Horizontal Scrollbar
         textPane.setText(printWelcomeMessage());
         // Panel at bottom with label, text field, and buttons
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Github URL: ");
         JTextField textField = new JTextField(30); // Accepts up to 30 characters
-        JButton btn_send = new JButton("Fetch & Analyze");
+
+        JButton btn_send = new JButton("Fetch & Analyze");  // Analyze button
         
-        JButton btn_reset = new JButton("Reset");
+        JButton btn_reset = new JButton("Reset");   // Reset button
 
         btn_send.setToolTipText("Fetch and analyze source code from the provided GitHub URL.");
         btn_reset.setToolTipText("Clear the GitHub URL field and the output area.");
@@ -73,86 +77,102 @@ class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String url = textField.getText().trim();
-                if (validateUrl(url)) {
+                if (validateUrl(url)) {     // If url is valid start process
 
-                    // Loading dialog
+                    // Loading screen
                     JDialog loadingDialog = new JDialog(jframe, "SAT Tool", true);
                     loadingDialog.setLayout(new BorderLayout());
-                    loadingDialog.add(new JLabel("Analyzing, please wait...", SwingConstants.CENTER), BorderLayout.CENTER);
-                    loadingDialog.setSize(200, 100);
+                    loadingDialog.add(new JLabel("Analyzing, please wait...", SwingConstants.CENTER), BorderLayout.CENTER);     // Contents
+                    loadingDialog.setSize(200, 100);    // Size of loading screen
                     loadingDialog.setLocationRelativeTo(jframe);
 
                     JProgressBar progressBar = new JProgressBar();
                     progressBar.setIndeterminate(true);
                     loadingDialog.add(progressBar, BorderLayout.SOUTH);
 
+                    // Use SwingWorker to create a loading screen while cloning & analyzing run in the background
                     SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
                         @Override
                         protected Void doInBackground() {
-                            sourceLocation.getSourceCode(url);
-                            sourceLocation.analyzeSourceCode();
-                            try (Scanner myReader = new Scanner(new File(sourceLocation.getOutFile()))) {
-                                HashMap<String, ArrayList<String>> categorizedWarnings = new HashMap<>();
-                                while (myReader.hasNextLine()) {
-                                    String data = myReader.nextLine();
-                                    Pattern pattern = Pattern.compile("(?<=\\t)[A-Za-z]+(?=:\\s)");
-                                    Matcher matcher = pattern.matcher(data);
-                                    if (matcher.find()) {
-                                        String warningType = matcher.group();
-                                        categorizedWarnings.putIfAbsent(warningType, new ArrayList<>());
-                                        categorizedWarnings.get(warningType).add(data);
+                                sourceLocation.getSourceCode(url);      // clone a Git repo locally
+                                sourceLocation.analyzeSourceCode(); // Run the PMD on the cloned repo
+
+                            
+                                try (Scanner myReader = new Scanner(new File(sourceLocation.getOutFile()))) {    // Read the results.csv file, catch any errors when opening the file
+                                    HashMap<String, ArrayList<String>> categorizedWarnings = new HashMap<>();   // A hashmap that will store the warnings after they are categorized
+                                    while (myReader.hasNextLine()) {
+                                        String data = myReader.nextLine();
+
+
+                                        // This regex pattern matches strings that:
+                                        // 1. Are preceded by a tab character (lookbehind: (?<=\\t))
+                                        // 2. Consist of one or more alphabetic characters (A-Z, a-z)
+                                        // 3. Are followed by a colon and a whitespace (lookahead: (?=:\\s))
+                                        // The matched part will be the alphabetic characters only.
+                                        Pattern pattern = Pattern.compile("(?<=\\t)[A-Za-z]+(?=:\\s)");     
+                                        Matcher matcher = pattern.matcher(data);
+                                        if (matcher.find()) {
+                                                String warningType = matcher.group();
+                                                categorizedWarnings.putIfAbsent(warningType, new ArrayList<>());
+                                                categorizedWarnings.get(warningType).add(data);
+                                        }
                                     }
-                                }
 
-                                doc.insertString(doc.getLength(), "\nAnalyzing file: " + sourceLocation.getOutFile() + "\n\n", null);
+                                    doc.insertString(doc.getLength(), "\nAnalyzing file: " + sourceLocation.getOutFile() + "\n\n", null);
 
-                                // Display categorized warnings with color-coding based on risk level
-                                for (String warningType : categorizedWarnings.keySet()) {
-                                    ArrayList<String> warnings = categorizedWarnings.get(warningType);
-                                    SimpleAttributeSet set = new SimpleAttributeSet();
-                                    
-                                    doc.insertString(doc.getLength(), "\n" + warningType + " (" + warnings.size() + " occurrences):\n", set);
-                                    for (String warning : warnings) {
-                                        doc.insertString(doc.getLength(), warning + "\n", set);
+                                    // Display warnings from the hashmap, count the size of each warnings/violations and display it
+                                    for (String warningType : categorizedWarnings.keySet()) {
+                                            ArrayList<String> warnings = categorizedWarnings.get(warningType);
+                                            SimpleAttributeSet set = new SimpleAttributeSet();
+                                            doc.insertString(doc.getLength(), "\n" + warningType + " (" + warnings.size() + " occurrences):\n", set);   // Get the size of each warning and display it
+                                            int totalWarnings = 0;
+                                            
+                                            // Display first 10 violation occurrences 
+                                            for (String warning : warnings) {
+                                                    if (totalWarnings < 10){
+                                                            doc.insertString(doc.getLength(), warning + "\n", set);
+                                                            totalWarnings++;
+                                                    }
+                                            }
+            
                                     }
+
+                                } catch (FileNotFoundException ex) {
+                                        JOptionPane.showMessageDialog(jframe, "Error: Output file not found!", "Error", JOptionPane.ERROR_MESSAGE);
+                                        ex.printStackTrace();
+                                } catch (Exception ex) {
+                                        ex.printStackTrace();
                                 }
-
-                            } catch (FileNotFoundException ex) {
-                                JOptionPane.showMessageDialog(jframe, "Error: Output file not found!", "Error", JOptionPane.ERROR_MESSAGE);
-                                ex.printStackTrace();
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
+                                return null;
                             }
-                            return null;
-                        }
-
-                        @Override
-                        protected void done() {
-                            try {
-                                get();
-                            } catch (InterruptedException | ExecutionException ex) {
-                                ex.printStackTrace();
+                            // Override the method done() to perform completion tasks
+                            @Override
+                            protected void done() {
+                                try {
+                                    get();
+                                } catch (InterruptedException | ExecutionException ex) {
+                                    ex.printStackTrace();
+                                }
+                                loadingDialog.dispose();
                             }
-                            loadingDialog.dispose();
-                        }
-                    };
+                        };
 
-                    SwingUtilities.invokeLater(() -> loadingDialog.setVisible(true));
-                    worker.execute();
+                        SwingUtilities.invokeLater(() -> loadingDialog.setVisible(true));
+                        worker.execute();   // Start the execution on a 'worker' thread
 
-                } else {
-                    JOptionPane.showMessageDialog(jframe, "Please enter a valid GitHub URL.", "Invalid URL", JOptionPane.WARNING_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(jframe, "Please enter a valid GitHub URL.", "Invalid URL", JOptionPane.WARNING_MESSAGE);
+                    }
                 }
-            }
-        });
+            });
 
         // Reset button action
         btn_reset.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("");
-                textPane.setText(printWelcomeMessage());
-            }
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    textField.setText("");      // Reset the GUI
+                    textPane.setText(printWelcomeMessage());    // Print the welcome message
+                }
         });
 
         panel.add(label);
@@ -167,29 +187,29 @@ class GUI {
     }
 
 
-
+    // return a welcome message string
     private String printWelcomeMessage() {
-        StringBuilder string = new StringBuilder();
-        string.append("                                                                                   \n");
-        string.append("                            Welcome to the Static Analysis Tool                      \n");
-        string.append("===================================================================================\n");
-        string.append("This application performs static code analysis to identify potential issues, naming   \n");
-        string.append("convention violations, and other coding standards within your Java projects. The    \n");
-        string.append("analysis results are saved to a designated file, which can be viewed in the GUI.    \n\n");
-        string.append("Usage Instructions:                                                                \n");
-        string.append("1. Enter a valid GitHub repository URL in the text field provided.                 \n");
-        string.append("2. Click the 'Fetch & Analyze' button to initiate the analysis.                    \n\n");
-        string.append("Important Note:                                                                    \n");
-        string.append("All source files are stored in the 'C://SAT-TESTS' directory. Folders are named    \n");
-        string.append("based on their clone date in the format: yyyy-MM-dd-HH-mm-ssSS.                          \n\n");
-        string.append("Developer: Nour Alhouseini                                                          \n");
-        string.append("===================================================================================\n");
-        return string.toString();
+            StringBuilder string = new StringBuilder();
+            string.append("                                                                                   \n");
+            string.append("                            Welcome to the Static Analysis Tool                      \n");
+            string.append("===================================================================================\n");
+            string.append("This application performs static code analysis to identify potential issues, naming   \n");
+            string.append("convention violations, and other coding standards within your Java projects. The    \n");
+            string.append("analysis results are saved to a designated file, which can be viewed in the GUI.    \n\n");
+            string.append("Usage Instructions:                                                                \n");
+            string.append("1. Enter a valid GitHub repository URL in the text field provided.                 \n");
+            string.append("2. Click the 'Fetch & Analyze' button to initiate the analysis.                    \n\n");
+            string.append("Important Note:                                                                    \n");
+            string.append("All source files are stored in the 'C://SAT-TESTS' directory. Folders are named    \n");
+            string.append("based on their clone date in the format: yyyy-MM-dd-HH-mm-ssSS.                          \n\n");
+            string.append("Developer: Nour Alhouseini                                                          \n");
+            string.append("===================================================================================\n");
+            return string.toString();
     }
     
     
-
-    private boolean validateUrl(String url) {
-        return url.startsWith("https://github.com/") && url.split("/").length >= 5;
+    // Returns true if the URL starts with "https://github.com/" and has at least 5 segments when split by "/", false otherwise. 
+    public boolean validateUrl(String url) {
+            return url.startsWith("https://github.com/") && url.split("/").length >= 5;
     }
 }
