44108cd23a9ce69542f82a453d683a4e66743928
Nour Alhouseini <86202341+0xLumos@users.noreply.github.com>
2026-02-13 20:13:46 -0600

    Delete sta-prog directory v1

--- sta-prog/src/main/java/com/sta/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sat;
-
-public class Main {
-
-    public static void main(String[] args) {
-        SourceAnalyzer source = new SourceAnalyzer();
-        GUI gui = new GUI(source);
-    }
-}
